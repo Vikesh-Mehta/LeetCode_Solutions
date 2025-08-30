457f0864ce30803090448731fa260f308580fb25
Vikesh Mehta <148038933+Vikesh-Mehta@users.noreply.github.com>
2025-08-30 13:05:43 +0530

    Time: 53 ms (45.57%) | Memory: 90.9 MB (32.42%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> sett;
+        for(int num : nums){
+            if(sett.count(num)){
+                return true;
+            }
+            sett.insert(num);
+        }
+        return false;
+    }
+};
